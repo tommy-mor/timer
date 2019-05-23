3f47f55fea38729e0487f34fd21a4ceed3cdbf5f
Tommy Morriss <thmorriss@gmail.com>
2019-05-22 18:31:53 -0700

    addedd moment.js

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 
-  // DOM element where the Timeline will be attached
-  var container = document.getElementById('visualization');
+// DOM element where the Timeline will be attached
+var container = document.getElementById('visualization');
 
-  // Create a DataSet (allows two way data-binding)
-  var items = new vis.DataSet([
-    {id: 1, content: 'item 1', start: '2013-04-20'},
-    {id: 2, content: 'item 2', start: '2013-04-14'},
-    {id: 3, content: 'item 3', start: '2013-04-18'},
-    {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
-    {id: 5, content: 'item 5', start: '2013-04-25'},
-    {id: 6, content: 'item 6', start: '2013-04-27'}
-  ]);
+// Create a DataSet (allows two way data-binding)
+var items = new vis.DataSet([
+    { id: 1, content: 'item 1', start: '2013-04-20' },
+    { id: 2, content: 'item 2', start: '2013-04-14' },
+    { id: 3, content: 'item 3', start: '2013-04-18' },
+    { id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19' },
+    { id: 5, content: 'item 5', start: '2013-04-25' },
+    { id: 6, content: 'item 6', start: '2013-04-27' }
+]);
 
-  // Configuration for the Timeline
-  var options = {};
+var early = new Date();
+early.setHours(0)
+early.setMinutes(0)
+early.setSeconds(0)
+early.setMilliseconds(0)
+var late = (new Date()).setDate(early) + 1;
+// Configuration for the Timeline
+var options = {
+    editable: true,
+    min: early,
+    max: late
+};
 
-  // Create a Timeline
-  var timeline = new vis.Timeline(container, items, options);
+function press() {
+    console.log('strst')
+}
+
+// Create a Timeline
+var timeline = new vis.Timeline(container, items, options);

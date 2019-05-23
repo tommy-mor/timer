1e4dd67f75f6a8f7de032f25cb0b92bb08fe1dae
Tommy Morriss <thmorriss@gmail.com>
2019-05-22 18:37:18 -0700

    made it set to today

--- index.js
+++ index.js
@@ -4,25 +4,13 @@ var container = document.getElementById('visualization');
 
 // Create a DataSet (allows two way data-binding)
 var items = new vis.DataSet([
-    { id: 1, content: 'item 1', start: '2013-04-20' },
-    { id: 2, content: 'item 2', start: '2013-04-14' },
-    { id: 3, content: 'item 3', start: '2013-04-18' },
-    { id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19' },
-    { id: 5, content: 'item 5', start: '2013-04-25' },
-    { id: 6, content: 'item 6', start: '2013-04-27' }
 ]);
 
-var early = new Date();
-early.setHours(0)
-early.setMinutes(0)
-early.setSeconds(0)
-early.setMilliseconds(0)
-var late = (new Date()).setDate(early) + 1;
 // Configuration for the Timeline
 var options = {
     editable: true,
-    min: early,
-    max: late
+    min: moment().startOf('day'),
+    max: moment().startOf('day').add(1, 'days')
 };
 
 function press() {
